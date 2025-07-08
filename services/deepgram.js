5c4d2bf9bb152fb77d8219fa8648d1359d4c985d
ES5210 <e.sevenich@outlook.com>
2025-07-08 13:15:01 +0200

    Update deepgram.js

--- services/deepgram.js
+++ services/deepgram.js
@@ -1,5 +1,25 @@
-async function handleSpeechToText(req) {
-  const speechResult = req.body.SpeechResult;
-  return speechResult || "Ich habe dich nicht verstanden.";
+const axios = require("axios");
+
+async function handleSpeechToText(recordingUrl) {
+  const deepgramApiKey = process.env.DEEPGRAM_API_KEY;
+
+  const response = await axios({
+    method: "POST",
+    url: "https://api.deepgram.com/v1/listen",
+    headers: {
+      Authorization: `Token ${deepgramApiKey}`,
+      "Content-Type": "application/json"
+    },
+    data: {
+      url: `${recordingUrl}.mp3`, // Twilio gibt nur Base-URL zurück!
+      language: "de",
+      model: "nova",
+      smart_format: true
+    }
+  });
+
+  const transcript = response.data.results.channels[0].alternatives[0].transcript;
+  return transcript || "Keine Sprache erkannt";
 }
+
 module.exports = { handleSpeechToText };
